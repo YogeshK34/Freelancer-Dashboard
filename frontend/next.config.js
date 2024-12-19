2bf9c6e484db6d2466446877c19a3a73e1b68dab
Yogesh Khutwad <khutwadyogesh34@gmail.com>
2024-12-19 18:50:22 +0530

    updated next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['wallpaperaccess.com', 'cdn.vox-cdn.com', 'static.learnk8s.io', 'images.unsplash.com', 'th.bing.com', 'wallpapercave.com', 'images.hdqwalls.com'],
     },
+    reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    }
 }
 
 module.exports = nextConfig
